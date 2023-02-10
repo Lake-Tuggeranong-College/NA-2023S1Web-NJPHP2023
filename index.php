e7349325dbcd85f33e224fc755cd690aeecb2652
nate-jon <4200172@schoolsnet.act.edu.au>
2023-02-10 14:16:18 +1100

    index update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" conytent="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PHP Template</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
@@ -11,5 +11,5 @@
 <h1>Heading</h1>
 
 </body>
-<script scr="js/bootstrap.bundle.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
 </html>
