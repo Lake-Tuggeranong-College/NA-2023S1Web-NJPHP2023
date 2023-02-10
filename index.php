aedbd7c515f6bcb9affd6638239d183f1ca32f40
nate-jon <4200172@schoolsnet.act.edu.au>
2023-02-10 14:15:25 +1100

    index update

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<?php include "template.php"?>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" conytent="width=device-width, initial-scale=1">
+    <title>PHP Template</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<h1>Heading</h1>
+
+</body>
+<script scr="js/bootstrap.bundle.min.js"></script>
+</html>
