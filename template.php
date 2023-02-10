499afd823c2d2d36bb805d0dd95f2d53d24c1fa7
nate-jon <4200172@schoolsnet.act.edu.au>
2023-02-10 14:27:02 +1100

    saniteses data

--- template.php
+++ template.php
@@ -1,9 +1,17 @@
 <?php
 
 function footer():string {
+    date_default_timezone_set('Australia/Canberra');
     $filename = basename($_SERVER["SCRIPT_FILENAME"]);
-    $footer = "This page was last modified: " . date( format: "F d Y H:i:s.", filemtime($filename));
+    $footer = "This page was last modified: " . date( "F d Y H:i:s.", filemtime($filename));
     return $footer;
+}
+
+function ($unsanitisedData) {
+    $unsanitisedData = trim($unsanitisedData);
+    $unsanitisedData = stripslashes($unsanitisedData);
+    $sanitisedData = htmlspecialchars($unsanitisedData);
+    return $sanitisedData;
 }
 
 ?>
