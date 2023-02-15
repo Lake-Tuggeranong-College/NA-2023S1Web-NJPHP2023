cd5c05a07ff09fb2e98ec4fa3eb0f12fa862d2db
nate-jon <4200172@schoolsnet.act.edu.au>
2023-02-15 11:52:53 +1100

    impruved imput and output function

--- contact.php
+++ contact.php
@@ -1,5 +1,4 @@
 <!doctype html>
-<?php include "template.php"?>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -8,40 +7,44 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-
-
 <h1>Contact us</h1>
+
 <div class="container-fluid">
-    <h1 class="text-primary">Please Send us a Message</h1>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <form action="contact.php" method="post">
         <div class="mb-3">
-            <label for="contactEmail" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="contactEmail" name="contactEmail"
-                   placeholder="name@example.com">
+            <label for="inputEmail" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="inputEmail" name="inputEmail" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
-            <label for="contactMessage" class="form-label">Message</label>
-            <textarea class="form-control" id="contactMessage" name="contactMessage" rows="3"></textarea>
+            <label for="inputMessage" class="form-label">Message</label>
+            <input type="text" class="form-control" id="inputMessage" name="inputMessage">
         </div>
-        <button type="submit" name="formSubmit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
 
 <?php
-if (isset($_POST['formSubmit'])) {
-    $userEmail = sanitisedData (['contactEmail']);
-    $userMessage = sanitisedData ($_POST['contactMessage']);
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $formError = false;
+    if (empty($_POST['inputEmail'])) {
+        $formError = true;
+        echo "Enter an email address.";
+    }
+    if (empty($_POST['inputMessage'])) {
+        $formError = true;
+        echo "Enter amessage to submit.";
+    }
+    if ($formError == false) {
+        $emailAddress =$_POST['inputEmail'];
+        $messageSubmitted = $_POST['inputMessage'];
 
-    echo $userEmail;
-    echo "<p>";
-    echo $userMessage;
+        echo $emailAddress;
+        echo "<p>";
+        echo $messageSubmitted;
+    }
 }
-
 ?>
-
-
-
-<?php echo footer() ?>
 </body>
 <script src="js/bootstrap.bundle.min.js"></script>
 </html>
